7401152e03f98dc586f701b8fa82df4f016c56fc
Kim MyungSub <audtjqxx@naver.com>
2023-12-18 05:32:12 +0900

    backdrop 닫기 event 처리

--- src/components/common/AlertModal/BackDrop.tsx
+++ src/components/common/AlertModal/BackDrop.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import * as St from './alertModal.styled';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../../redux/config';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../../redux/config';
+import { closeAlert } from '../../../redux/modules/AlertSlice';
 
 const BackDrop = () => {
-  const checkOpen = useSelector((state: RootState) => state.AlertSlice.isOpen);
-  return <St.BackDropContainer $isOpen={checkOpen} />;
+  const alertInfo = useSelector((state: RootState) => state.AlertSlice);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const backDropClickHandler = () => {
+    dispatch(closeAlert());
+  };
+  return <St.BackDropContainer $isOpen={alertInfo.isOpen} onClick={backDropClickHandler} />;
 };
 
 export default BackDrop;
